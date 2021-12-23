43c3176a3e049b3c2d7aa5ccfdc8dac3b617af3f
Jose Roldan <jose.roldan.colaborador@prf.gov.br>
2021-12-22 23:28:37 -0300

    First commit

---
+++ src/main/java/ar/com/egomusic/demogl/DemoglApplication.java
@@ -0,0 +1,13 @@
+package ar.com.egomusic.demogl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoglApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/egomusic/demogl/DemoglApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.egomusic.demogl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoglApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
